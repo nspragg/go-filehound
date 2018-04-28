50947d09a2b9019f824a5648859039dd99ffdcab
nick spragg <5319732+nspragg@users.noreply.github.com>
2018-04-28 15:15:18 +0100

    Task: Add runnable examples.

--- filehound/example_test.go
+++ filehound/example_test.go
@@ -2,22 +2,52 @@ package filehound_test
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/nspragg/go-filehound/filehound"
 )
 
-func ExampleExt() {
-	fh := filehound.New()
-	fh.Query(filehound.Ext(".txt"))
-	files := fh.Find()
+func ExamplePath() {
+	client := filehound.New()
+	client.Query(filehound.Path("./fixtures/examples"))
+	files := client.Find()
 
-	fmt.Println(files)
+	fmt.Printf("%d files found\n", len(files))
+
+	// Output: 2 files found
+}
+
+func ExampleExt() {
+	client := filehound.New()
+	client.Query(filehound.Path("./fixtures/examples"))
+	client.Query(filehound.Ext(".json"))
+	files := client.Find()
+
+	fmt.Println(filepath.Base(files[0]))
+
+	// Output: example.json
+}
+
+func ExampleGlob() {
+	client := filehound.New()
+	client.Query(filehound.Path("./fixtures/examples"))
+	client.Query(filehound.Glob("*.txt"))
+	files := client.Find()
+
+	fmt.Printf("%d file found\n", len(files))
+
+	// Output: 1 file found
 }
 
 func ExampleSize() {
-	fh := filehound.New()
-	fh.Query(filehound.Size(1024))
-	files := fh.Find()
+	bytes := int64(33)
 
-	fmt.Println(files)
+	client := filehound.New()
+	client.Query(filehound.Path("./fixtures/examples"))
+	client.Query(filehound.Size(bytes))
+	files := client.Find()
+
+	fmt.Printf("%d file found that is %d bytes in length\n", len(files), bytes)
+
+	// Output: 1 file found that is 33 bytes in length
 }
